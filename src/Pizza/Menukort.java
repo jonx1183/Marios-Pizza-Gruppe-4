cae465c10d9b4e7776f2319e95185b3581852a04
jonx1183 <jonx1183@stud.kea.dk>
2022-04-20 10:52:40 +0200

    Added pizza to menu

--- src/Pizza/Menukort.java
+++ src/Pizza/Menukort.java
@@ -1,9 +1,26 @@
 package Pizza;
 
+import java.util.ArrayList;
+
 public class Menukort {
 
-    Menukort(){
+  public void pizzaer(){
+    ArrayList<String> pizza = new ArrayList<String>();
 
-    }
+    pizza.add("Vesuvio");
+    pizza.add("Amerikaner");
+    pizza.add("Cacceatore");
+    pizza.add("Carbona");
+    pizza.add("Dennis");
+    pizza.add("Beril");
+    pizza.add("Silvia");
+    pizza.add("Victoria");
+    pizza.add("Toronfo");
+    pizza.add("Capricciosa");
+    pizza.add("Hawai");
+    pizza.add("Le Blissola");
+    pizza.add("Venezia");
+    pizza.add("Mafia");
+  }
 
 }
