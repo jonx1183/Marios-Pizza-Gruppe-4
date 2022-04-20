8a58270de83d16eaa86299e48cb46983229ef589
jonx1183 <jonx1183@stud.kea.dk>
2022-04-20 10:52:40 +0200

    Added pizza to menu

--- src/Pizza/Main.java
+++ src/Pizza/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
     void go(){
 
+        Menukort menukort = new Menukort();
+        menukort.pizzaer();
+
     }
 
     public static void main(String[] args) {
         new Main().go();
+
     }
 }
--- src/Pizza/Menukort.java
+++ src/Pizza/Menukort.java
@@ -1,9 +1,31 @@
 package Pizza;
 
+import java.util.ArrayList;
+
 public class Menukort {
 
-    Menukort(){
+  public void pizzaer(){
+    ArrayList<String> pizza = new ArrayList<String>();
 
-    }
+    pizza.add("Vesuvio" + "\n");
+    pizza.add("Amerikaner" + "\n");
+    pizza.add("Cacceatore" + "\n");
+    pizza.add("Carbona" + "\n");
+    pizza.add("Dennis" + "\n");
+    pizza.add("Beril" + "\n");
+    pizza.add("Silvia" + "\n");
+    pizza.add("Victoria" + "\n" );
+    pizza.add("Toronfo" + "\n");
+    pizza.add("Capricciosa" + "\n");
+    pizza.add("Hawai" + "\n");
+    pizza.add("Le Blissola" + "\n");
+    pizza.add("Venezia" + "\n");
+    pizza.add("Mafia"+ "\n");
+
+
+    System.out.println(pizza);
+
+
+  }
 
 }
